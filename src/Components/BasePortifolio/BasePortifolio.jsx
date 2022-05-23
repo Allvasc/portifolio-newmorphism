b53e5737ffc168d2c2664b0c8de4a3430343ed7c
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-05-23 19:01:17 -0300

    Add container Home

--- src/Components/BasePortifolio/BasePortifolio.jsx
+++ src/Components/BasePortifolio/BasePortifolio.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './BasePortifolio.css'
 import InfoComponent from '../InfoPortifolio/InfoPortifolio';
+import Home from '../Home/Home';
 
 const BasePortifolio = () => {
   return (
     <div className='baseContainer'>
         <InfoComponent></InfoComponent>
+        <Home></Home>
     </div>
   )
 }
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='caseHome'>
+            <p>Olá, Meu nome é</p>
+            <h1>Allison Vasconcelos</h1>
+            <h2>Desenvolvedor Front end</h2>
+            <p>Especializado em construir(e as vezes desenhar) experiências digitais exepicionais.
+                Atualmente estou focado na construção de aplicações web responsivas full-stack</p>
+        </div>
+    )
+}
+
+export default Home
