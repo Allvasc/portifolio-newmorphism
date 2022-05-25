2bdf4c17426fd9117e6ecbde8b6cd1ed6cf6df2d
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-05-25 12:25:47 -0300

    sessão about implementada

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,7 +3,18 @@ import './About.css'
 
 const About = () => {
   return (
-    <div className='About'>About</div>
+    <div className='About'>
+      <h1>About</h1>
+      <p className='paragraph'>
+        Graduado em Análise e desenvolvimentos de sistemas para internet desde 2021.
+        Possuo experiência de +3 anos como Frontend, desenvolvendo sites, peças publicitárias,
+        landing pages, lojas virtuais e sistemas com niveis de acesso.<br/>
+        Sou apaixonado por desenvolvimento e design trends, sem deixar de lado as bosas práticas,
+        mobilefirst e acessibilidade. Possuo experiência com SEO, pixels e configurações de metatags.
+
+      </p>
+
+    </div>
   )
 }
 
